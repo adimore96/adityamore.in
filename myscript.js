47dd21ef7e0f3e815aa4ec3d2c73524e7e390814
adimore96 <91413755+adimore96@users.noreply.github.com>
2022-07-22 12:40:44 +0530

    aditya demo

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function setNewImage(){
+    document.getElementById("yteffect").src="/youtubered.png";
+    document.getElementById("myH2").style.color = "#ff0000";
+}
+function setOldImage(){
+    document.getElementById("yteffect").src="/youtube.png" 
+    document.getElementById("myH2").style.color = "white";
+
+}
